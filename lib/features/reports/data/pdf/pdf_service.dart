ac738fbdefbe442143837f7ce0c55e611579b036
rushild31 <rushilrckz@gmail.com>
2026-07-17 01:18:41 +0530

    Fix PdfService API and Uint8List compatibility

--- lib/features/reports/data/pdf/pdf_service.dart
+++ lib/features/reports/data/pdf/pdf_service.dart
@@ -2,35 +2,24 @@ import 'dart:typed_data';
 
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-
 import 'package:printing/printing.dart';
 
 class PdfService {
+  Future<Uint8List> generatePdf({required List<pw.Widget> content}) async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.MultiPage(pageFormat: PdfPageFormat.a4, build: (context) => content),
+    );
+
+    return pdf.save();
+  }
+
   Future<void> previewPdf({required Uint8List pdfBytes}) async {
     await Printing.layoutPdf(
       onLayout: (PdfPageFormat format) async {
         return pdfBytes;
       },
     );
-  }
-
-  Future<Uint8List> generatePdf({
-    required List<pw.Widget> content,
-
-    PdfPageFormat pageFormat = PdfPageFormat.a4,
-  }) async {
-    final pdf = pw.Document();
-
-    pdf.addPage(
-      pw.MultiPage(
-        pageFormat: pageFormat,
-
-        build: (context) {
-          return content;
-        },
-      ),
-    );
-
-    return pdf.save();
   }
 }
